13ee9f6d5396c5db62a7b295115cba0264cf890c
theotaylor <theotaylor2000@gmail.com>
2024-09-01 12:58:50 -0400

    uibuild

--- notes-backend/mongo.js
+++ notes-backend/mongo.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
-const url =
-  `mongodb+srv://theo:${password}@fullstack.uun7n.mongodb.net/nodeApp?retryWrites=true&w=majority&appName=fullstack`
+// const url =
+//   `mongodb+srv://theo:${password}@fullstack.uun7n.mongodb.net/nodeApp?retryWrites=true&w=majority&appName=fullstack`
 
-mongoose.set('strictQuery',false)
+// mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-// const note = new Note({
-//   content: 'mongoose makes things easy',
-//   important: true,
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
 // })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+// const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+// // const note = new Note({
+// //   content: 'mongoose makes things easy',
+// //   important: true,
+// // })
+
+// // note.save().then(result => {
+// //   console.log('note saved!')
+// //   mongoose.connection.close()
+// // })
+
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
